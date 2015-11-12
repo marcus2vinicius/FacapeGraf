0410e6c6285cd13f647f5edaef97957c109d0e08
felipe <felipe@felipe-PC>
2015-11-12 00:57:23 -0300

    algoritmo de dijkstra funcionando para grafos valorados. alterar para funcionar com digrafos. adaptado de: <http://www.vogella.com/tutorials/JavaAlgorithmsDijkstra/article.html>

---
+++ src/test/java/Dijkstra1.java
@@ -0,0 +1,123 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.management.RuntimeErrorException;
+
+import br.com.facapegraf.model.Aresta;
+import br.com.facapegraf.model.Grafo;
+import br.com.facapegraf.model.Vertice;
+
+public class Dijkstra1 {
+	
+	private final List<Vertice> vertices;
+	private final List<Aresta> arestas;
+	private Set<Vertice> verticesVerificados;
+	private Set<Vertice> verticesNaoVerificados;
+	private Map<Vertice, Vertice> predecessores;
+	private Map<Vertice, Integer> distancia;
+	
+	public Dijkstra1(Grafo grafo){
+		this.vertices = new ArrayList<Vertice>(grafo.getVertices());
+		this.arestas = new ArrayList<Aresta>(grafo.getArestas());
+	}
+	
+	public void run(Vertice fonte){
+		verticesVerificados = new HashSet<Vertice>();
+		verticesNaoVerificados = new HashSet<Vertice>();
+		distancia = new HashMap<Vertice, Integer>();
+		predecessores = new HashMap<Vertice, Vertice>();
+		
+		distancia.put(fonte, 0);
+		verticesNaoVerificados.add(fonte);
+		
+		while(verticesNaoVerificados.size() > 0){
+			Vertice v = getfMinimo(verticesNaoVerificados);
+			verticesVerificados.add(v);
+			verticesNaoVerificados.remove(v);
+			findDistanciasMinimas(v);
+		}
+	}
+	
+	private void findDistanciasMinimas(Vertice vertice){
+		List<Vertice> verticesAdjacentes = getVizinhos(vertice);
+		for(Vertice adj : verticesAdjacentes){
+			if (getMenorDistancia(adj) > getMenorDistancia(vertice) + 
+										 getDistancia(vertice, adj)) {
+				distancia.put(adj, getMenorDistancia(vertice) + 
+						           getDistancia(vertice, adj));
+				predecessores.put(adj, vertice);
+				verticesNaoVerificados.add(adj);
+			}
+		}
+	}
+	
+	private int getDistancia(Vertice v1, Vertice v2){
+		for(Aresta aresta : arestas){
+			if (aresta.getV1().equals(v1) && aresta.getV2().equals(v2)) {
+				return aresta.getPeso();
+			}
+		}
+		throw new RuntimeException("Erro");
+	}
+	
+	private List<Vertice> getVizinhos(Vertice vertice){
+		List<Vertice> vizinhos = new ArrayList<Vertice>();
+		for(Aresta aresta : arestas){
+			if (aresta.getV1().equals(vertice) && !isVerificado(aresta.getV2())) {
+				vizinhos.add(aresta.getV2());
+			}
+		}
+		return vizinhos;
+	}
+	
+	
+	private Vertice getfMinimo(Set<Vertice> vertices){
+		Vertice minimo = null;
+		for(Vertice vertice : vertices){
+			if(minimo == null){
+				minimo = vertice;
+			} else {
+				if (getMenorDistancia(vertice) < getMenorDistancia(minimo)) {
+					minimo = vertice;
+				}
+			}
+		}
+		return minimo;
+	}
+	
+	private boolean isVerificado(Vertice vertice){
+		return this.verticesVerificados.contains(vertice);
+	}
+	
+	private int getMenorDistancia(Vertice destino){
+		Integer d = distancia.get(destino);
+		if (d == null) {
+			return Integer.MAX_VALUE;
+		} else {
+			return d;
+		}
+	}
+	
+	public LinkedList<Vertice> getCaminho(Vertice alvo){
+		LinkedList<Vertice> caminho = new LinkedList<Vertice>();
+		Vertice temp = alvo;
+		
+		if (predecessores.get(temp) == null) {
+			return null;
+		}
+		
+		caminho.add(temp);
+		while(predecessores.get(temp) != null){
+			temp = predecessores.get(temp);
+			caminho.add(temp);
+		}
+		Collections.reverse(caminho);
+		return caminho;
+	}
+}
---
+++ src/test/java/TestDijkstra1.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import br.com.facapegraf.leitu.ConstruGraf;
+import br.com.facapegraf.model.Aresta;
+import br.com.facapegraf.model.Vertice;
+
+public class TestDijkstra1 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+//        Grafo g = new Grafo();
+        List<Aresta> arestas = new ArrayList<Aresta>();
+        HashMap<Aresta, Integer> distancias = new HashMap<Aresta, Integer>();
+
+        
+        String caminho = new File("").getAbsoluteFile() + 
+        		Constantes.url_matriz_exemplos+"/matriz1.txt";
+        ConstruGraf cg = new ConstruGraf(caminho);
+        
+        arestas = cg.getGrafo().getArestas();
+        List<Vertice> vertices = cg.getGrafo().getVertices();
+        
+        System.out.println(cg.matrizToString());
+        for(Aresta a : arestas){
+        	System.out.println("Aresta " + a.getV1() + " - " + a.getV2() + " -> " + a.getPeso());
+        }
+        
+        Dijkstra1 dijkstra = new Dijkstra1(cg.getGrafo());
+        
+        //vertice fonte
+        dijkstra.run(vertices.get(0));
+        
+        //coloca o vertice destino como parametro
+        LinkedList<Vertice> menorCaminho = dijkstra.getCaminho(vertices.get(3));
+        System.out.println();
+        System.out.print("Caminho: ");
+        for(Vertice vertice : menorCaminho){
+        	System.out.print("-> " + vertice);
+        }
+    }
+
+}
